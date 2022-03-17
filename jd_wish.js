3e1b85bc025db4bdb6c0f49263cf331497291d8d
FengYun7 <Null@qq.com>
2022-03-17 19:02:50 +0800

    Update Wish

--- jd_wish.js
+++ jd_wish.js
@@ -7,7 +7,7 @@ cron "40 0,2 * * *"
 const $ = new Env('众筹许愿池');
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
-const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
+const jdCookieNode = $.isNode() ? require('./jdcookie.js') : '';
 let message = '', allMessage = '';
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '';
