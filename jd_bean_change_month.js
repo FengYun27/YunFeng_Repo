79eb2f4aa47f3ff462473695ea4d48b482508371
FengYun7 <windfgg@gmail.com>
2022-04-06 09:28:28 +0800

    Update fix

--- jd_bean_change_month.js
+++ jd_bean_change_month.js
@@ -10,7 +10,7 @@
 const $ = new Env('京东月资产变动通知');
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
-const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
+const jdCookieNode = $.isNode() ? require('./jdcookie.js') : '';
 let allMessage = '';
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '';
